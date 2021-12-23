9598c58a0cc1f7f347d59c02812195f25fc9da37
Neil Empey <argus.two.2@gmail.com>
2021-12-22 22:30:02 -0700

    openWeatherApi call success

--- app/models/route.rb
+++ app/models/route.rb
@@ -7,18 +7,16 @@ class Route < ApplicationRecord
     place1 = "#{self.lat1}, #{self.long1}"
     place2 = "#{self.lat2}, #{self.long2}"
 
-    puts place1
-    puts place2
-
     url = "http://www.mapquestapi.com/directions/v2/route?key=#{Rails.application.credentials.route[:consumer_key]}&from=#{place1}&to=#{place2}"
 
     response = HTTParty.get(url)
 
     array = response.parsed_response["route"]["legs"][0]["maneuvers"]
+    setOfCoordinates = array.map.with_index {|x, i| x["startPoint"]}
 
-    #forEach in array need to pull ["startPoint"]["lng"], ["startPoint"]["lat"]
+    #OpenWeatherApi example call response = HTTParty.get("https://api.openweathermap.org/data/2.5/onecall?lat=43.6166163&lon=-116.200886&units=imperial&exclude=alerts,minutely,hourly,daily&appid=22a302067ee032df09b48ac978a218e1")
 
-    puts response
+    puts setOfCoordinates
 
     end
 
